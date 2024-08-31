1c24cf0fa3d3a5ef6fdbb3b221afc6c0096d150d
stevenyu253436 <stevenyu.supreme@gmail.com>
2024-09-01 00:30:26 +0800

    Add isSymmetric.go implementation

---
+++ isSymmetric.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+/**
+ * Definition for a binary tree node.
+ */
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isSymmetric(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	return isMirror(root.Left, root.Right)
+}
+
+func isMirror(t1 *TreeNode, t2 *TreeNode) bool {
+	if t1 == nil && t2 == nil {
+		return true
+	}
+	if t1 == nil || t2 == nil {
+		return false
+	}
+	return (t1.Val == t2.Val) && isMirror(t1.Left, t2.Right) && isMirror(t1.Right, t2.Left)
+}
+
+func main() {
+	// 创建测试用的二叉树
+	root := &TreeNode{Val: 1}
+	root.Left = &TreeNode{Val: 2}
+	root.Right = &TreeNode{Val: 2}
+	root.Left.Left = &TreeNode{Val: 3}
+	root.Left.Right = &TreeNode{Val: 4}
+	root.Right.Left = &TreeNode{Val: 4}
+	root.Right.Right = &TreeNode{Val: 3}
+
+	// 调用函数并打印结果
+	fmt.Println(isSymmetric(root)) // 输出: true
+}
