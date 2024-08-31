49fb11821aea3772004b9e07cba203ca51efabf4
stevenyu253436 <stevenyu.supreme@gmail.com>
2024-09-01 00:56:42 +0800

    Implement lowestCommonAncestor to find the lowest common ancestor of two nodes in a binary tree

---
+++ lowestCommonAncestor.go
@@ -0,0 +1,53 @@
+package main
+
+import "fmt"
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root == nil || root == p || root == q {
+		return root
+	}
+
+	left := lowestCommonAncestor(root.Left, p, q)
+	right := lowestCommonAncestor(root.Right, p, q)
+
+	if left != nil && right != nil {
+		return root
+	}
+	if left != nil {
+		return left
+	}
+	return right
+}
+
+func main() {
+	// 示例测试
+	root := &TreeNode{Val: 3}
+	root.Left = &TreeNode{Val: 5}
+	root.Right = &TreeNode{Val: 1}
+	root.Left.Left = &TreeNode{Val: 6}
+	root.Left.Right = &TreeNode{Val: 2}
+	root.Right.Left = &TreeNode{Val: 0}
+	root.Right.Right = &TreeNode{Val: 8}
+	root.Left.Right.Left = &TreeNode{Val: 7}
+	root.Left.Right.Right = &TreeNode{Val: 4}
+
+	p := root.Left  // 节点5
+	q := root.Right // 节点1
+	ancestor := lowestCommonAncestor(root, p, q)
+	fmt.Printf("Lowest Common Ancestor of %d and %d is %d\n", p.Val, q.Val, ancestor.Val)
+}
