7918a34bcbf3505ad28e20d46bce57f206cec96f
stevenyu253436 <stevenyu.supreme@gmail.com>
2024-09-01 01:08:50 +0800

    Implement rightSideView to return the right side view of a binary tree

---
+++ rightSideView.go
@@ -0,0 +1,59 @@
+package main
+
+import "fmt"
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func rightSideView(root *TreeNode) []int {
+	if root == nil {
+		return []int{}
+	}
+
+	result := []int{}
+	queue := []*TreeNode{root}
+
+	for len(queue) > 0 {
+		levelSize := len(queue)
+		for i := 0; i < levelSize; i++ {
+			node := queue[0]
+			queue = queue[1:]
+			// 如果是當前層的最後一個節點，加入結果
+			if i == levelSize-1 {
+				result = append(result, node.Val)
+			}
+			// 將左子節點和右子節點依次加入隊列
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+		}
+	}
+
+	return result
+}
+
+func main() {
+	// 示例測試
+	root := &TreeNode{Val: 1}
+	root.Left = &TreeNode{Val: 2}
+	root.Right = &TreeNode{Val: 3}
+	root.Left.Right = &TreeNode{Val: 5}
+	root.Right.Right = &TreeNode{Val: 4}
+
+	fmt.Println(rightSideView(root)) // 輸出: [1, 3, 4]
+}
