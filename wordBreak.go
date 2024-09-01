98d4f0b04ee7dae17f452f62958c2b1fd7f79ce2
stevenyu253436 <stevenyu.supreme@gmail.com>
2024-09-02 05:25:58 +0800

    Implement wordBreak function to solve Word Break problem

---
+++ wordBreak.go
@@ -0,0 +1,20 @@
+func wordBreak(s string, wordDict []string) bool {
+	wordSet := make(map[string]bool)
+	for _, word := range wordDict {
+		wordSet[word] = true
+	}
+
+	dp := make([]bool, len(s)+1)
+	dp[0] = true
+
+	for i := 1; i <= len(s); i++ {
+		for j := 0; j < i; j++ {
+			if dp[j] && wordSet[s[j:i]] {
+				dp[i] = true
+				break
+			}
+		}
+	}
+
+	return dp[len(s)]
+}
