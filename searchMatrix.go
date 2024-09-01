721123969646199dd66d057de2ac55f69f6fba40
stevenyu253436 <stevenyu.supreme@gmail.com>
2024-09-01 12:44:16 +0800

    Add solution for Search a 2D Matrix problem

---
+++ searchMatrix.go
@@ -0,0 +1,25 @@
+func searchMatrix(matrix [][]int, target int) bool {
+	if len(matrix) == 0 || len(matrix[0]) == 0 {
+		return false
+	}
+
+	rows := len(matrix)
+	cols := len(matrix[0])
+
+	left, right := 0, rows*cols-1
+
+	for left <= right {
+		mid := left + (right-left)/2
+		midValue := matrix[mid/cols][mid%cols]
+
+		if midValue == target {
+			return true
+		} else if midValue < target {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+
+	return false
+}
