c71b822e5a3e09720d87922debc55d8905a142a6
stevenyu253436 <stevenyu.supreme@gmail.com>
2024-09-01 12:49:34 +0800

    Add search function for rotated sorted array problem

---
+++ search.go
@@ -0,0 +1,30 @@
+func search(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+
+	for left <= right {
+		mid := left + (right-left)/2
+
+		if nums[mid] == target {
+			return mid
+		}
+
+		// 判断哪一半是有序的
+		if nums[left] <= nums[mid] {
+			// 左半部分是有序的
+			if nums[left] <= target && target < nums[mid] {
+				right = mid - 1
+			} else {
+				left = mid + 1
+			}
+		} else {
+			// 右半部分是有序的
+			if nums[mid] < target && target <= nums[right] {
+				left = mid + 1
+			} else {
+				right = mid - 1
+			}
+		}
+	}
+
+	return -1
+}
